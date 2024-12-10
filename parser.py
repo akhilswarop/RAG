113d40ee696fb5368d1a18ba34f93b0fbe76e28f
Ganeshkaran M <ganeshkaran2008@gmail.com>
2024-12-10 22:06:01 +0530

    resume parsing updated

--- parser.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('C:/Users/swaro/OneDrive/Documents/GitHub/RAG/pyresparser')
-
-
-
-import pyresparser
-
-data = pyresparser.ResumeParser('Akhil_Swarop.docx').get_extracted_data()
-print(data)
