917e80e023d0f3f58d7f51b5764cffa29092cc78
Akhil Swarop S <96727867+akhilswarop@users.noreply.github.com>
2024-12-10 21:30:11 +0530

    Reverted degree list

--- pyresparser/constants.py
+++ pyresparser/constants.py
@@ -6,7 +6,7 @@ NAME_PATTERN = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
 # Education (Upper Case Mandatory)
 EDUCATION = [
             'BE', 'B.E.', 'B.E', 'BS', 'B.S', 'ME', 'M.E',
-            'M.E.', 'MS', 'M.S', 'B.TECH', 'M.TECH',
+            'M.E.', 'MS', 'M.S', 'BTECH', 'MTECH',
             'SSC', 'HSC', 'CBSE', 'ICSE', 'X', 'XII'
         ]
 
