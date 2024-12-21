d5767a18ae351ec7935c77e4b5dafb7773745b6d
Akhil Swarop S <96727867+akhilswarop@users.noreply.github.com>
2024-12-21 12:21:41 +0530

    Deleted unnecessary files

--- parser.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('C:/Users/swaro/OneDrive/Documents/GitHub/RAG/pyresparser')
-
-
-
-import pyresparser
-
-data = pyresparser.ResumeParser('White Turqoise Creative Simple Medical Personnel Nurse Resume.pdf').get_extracted_data()
-print(data)
